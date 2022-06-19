500be17d96d2d3cee57a9684d232062613e4ad70
Kopilov Aleksandr <kopilov.ad@gmail.com>
2022-06-19 22:02:08 +0300

    Include Arrow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <arrow/ipc/api.h>
+
 int main(int argc, char* argv[]) {//
-    std::cout << "Hello C++";
+    std::shared_ptr<arrow::io::FileOutputStream> out = arrow::io::FileOutputStream::Open("test.arrow").ValueOrDie();
+    //arrow::ipc::MakeStreamWriter(out);
+    //arrow::ipc::MakeFileWriter(out);
     return 0;
 }
